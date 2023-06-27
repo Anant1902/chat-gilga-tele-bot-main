44399f66d68bdce2f0b28aaed5164dc4185294a5
Anant Shanker <anant@MacBook-Air-3.local>
2023-06-27 14:23:54 +0800

    added intermittent domain pinging

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3003;
 const { connectAI } = require('./connectAI');
 require('dotenv').config();
 const { v4 } = require('uuid');
+const cron = require('node-cron');
+const https = require('https');
 
 app.get('/api', (req, res) => {
     const path = `/api/item/${v4()}`;
@@ -44,6 +46,26 @@ bot.on('message', async (msg) => {
     })
     console.log(ans);
     bot.sendMessage(msg.chat.id, ans);
+    });
+
+    cron.schedule('10 * * * * *', function() {
+        console.log('Running Cron Job');
+        https.get('https://chat-gilga-tele-bot.onrender.com', (resp) => {
+            let data = '';
+
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log('Successfully pinged domain');
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
     });
 
 module.exports = app
