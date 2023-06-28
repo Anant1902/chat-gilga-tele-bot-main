2cfef47a16825800bc812d8338b3c41d070d7047
Anant Shanker <anant@MacBook-Air-3.local>
2023-06-28 15:50:32 +0800

    cleaned up code

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ require('dotenv').config();
 const { v4 } = require('uuid');
 const cron = require('node-cron');
 const https = require('https');
+const mysql = require('mysql');
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-  });
+var con = mysql.createConnection({
+  host: "134.209.103.126",
+  user: "anant",
+  password: "uXkcYva4as",
+  database: "ai_chat"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 app.use(express.static('public'))
 
@@ -20,20 +26,32 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.tele_API_token;
 const bot = new TelegramBot(token, {polling: true});
 
-let messages = [
-]
-
-app.get('/testroute', (req, res) => {
-    res.json({
-        name: "Anant",
-        email: "shankeranant@gmail.com"
-    })
-  });
-
+let messages = []
+let id = '';
 
 bot.on('message', async (msg) => {
     let incoming_msg = msg.text.toString()
+    let firstName = msg.from.first_name;
+    let secondName = msg.from.last_name;
+    let userName = msg.from.username;
 
+
+    con.query( 'INSERT INTO users (firstName, secondName, userName, dateFirst, dateLast) VALUES (?, ?, ?, NOW(), NOW()) ON DUPLICATE KEY UPDATE dateLast=now()',
+            [firstName, secondName, userName], function (err, result, fields) {
+        if(err) console.log(err);
+        console.log("1 user record inserted");
+    });
+
+    con.query("INSERT INTO messages (username, userMessage, timeStamp, robotMessage) VALUES ( ?, ?, NOW(), null)", [userName, incoming_msg], function (err, result) {
+        if(err) console.log(err);
+        console.log("1 user message record inserted");
+        con.query("SELECT LAST_INSERT_ID() AS id", function (err, result) {
+            if(err) console.log(err);
+            id = result[0].id.toString();
+            console.log("Last insert id recorded");
+        });
+    });
+    
     messages.push({
         role: "user",
         content: incoming_msg
@@ -44,29 +62,36 @@ bot.on('message', async (msg) => {
         role: "assistant",
         content: ans
     })
-    console.log(ans);
+ 
+    con.query("UPDATE  messages SET timeStamp = NOW(), robotMessage =? WHERE id=?", [ans, id], function (err, result, fields) {
+        if(err) console.log(err);
+        console.log("1 ai message record inserted");
+    });
+
     bot.sendMessage(msg.chat.id, ans);
-    });
+});
 
-    cron.schedule('10 * * * * *', function() {
-        console.log('Running Cron Job');
-        https.get('https://chat-gilga-tele-bot.onrender.com', (resp) => {
-            let data = '';
 
-            // A chunk of data has been received.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+cron.schedule('10 * * * * *', function() {
+    console.log('Running Cron Job');
+    https.get('https://chat-gilga-tele-bot.onrender.com', (resp) => {
+        let data = '';
 
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                console.log('Successfully pinged domain');
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log('Successfully pinged domain');
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
     });
+});
+    
 
 module.exports = app
 app.listen(process.env.PORT || port, () => console.log(`Express app running on port ${port}!`));
